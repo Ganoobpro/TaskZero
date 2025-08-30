066c38f46dbc4c60efd960f0fa6e3ddbd5382e4d
Katcat <114319552+Ganoobpro@users.noreply.github.com>
2025-08-30 12:57:46 +0700

    Load file. Init handle command function.

--- taskzero.cpp
+++ taskzero.cpp
@@ -1,8 +1,50 @@
 ////////////////////  MAIN  ////////////////////
 #include "./taskzero.h"
 
-std::string command;
+TaskZero taskzero;
 
 int main() {
   return 0;
+}
+
+
+
+////////////////////  LOAD FILE  ////////////////////
+static void LoadFile() {
+  ifstream file(WORKLIST_FILE, ios::binary);
+
+  if (!file.good()) {
+    ofstream file("myfile.txt", ios::out);
+    file.close();
+    return;
+  }
+
+  Task new_task;
+  while (file.read((byte*)&new_task, sizeof(Task))) {
+    taskzero.push_back(new_task);
+  }
+}
+
+
+
+////////////////////  COMMANDS  ////////////////////
+static void CommandHandle() {
+  std::string command;
+
+  std::cout << "> ";
+  std::cin >> command;
+
+  if (command == "add") {
+    cout << "Adding task...\n";
+  } else if (command == "update") {
+    cout << "Updating task...\n";
+  } else if (command == "delete") {
+    cout << "Deleting task...\n";
+  } else if (command == "mark-in-progress") {
+    cout << "Marking task as in progress...\n";
+  } else if (command == "mark-done") {
+    cout << "Marking task as done...\n";
+  } else {
+    cout << "Unknown command.\n";
+  }
 }
--- taskzero.h
+++ taskzero.h
@@ -3,6 +3,43 @@
 
 #include "./setup.h"
 #include <string.h>
+#include <vector.h>
 #include <iostream>
+#include <stdint.h>
+
+#define WORKLIST_FILE "./worklist.bin"
+
+typedef byte char;
+
+
+
+////////////////////  TASK  ////////////////////
+typedef enum {
+  TASK_PENDING,
+  TASK_IN_PROGRESS,
+  TASK_DONE
+} TaskStatus;
+
+typedef enum {
+  NONURGENT_NOT_IMPORTANT,
+  NONURGENT_IMPORTANT,
+  URGENT_NOT_IMPORTANT,
+  URGENT_IMPORTANT
+} TaskPriority;
+
+typedef struct {
+  uint32_t id;
+  std::string task_name;
+  TaskStatus status;
+  TaskPriority priority;
+} Task;
+
+
+
+////////////////////  TASKZERO  ////////////////////
+typedef struct {
+  std::vector<Task> task_list;
+} TaskZero;
+
 
 #endif
